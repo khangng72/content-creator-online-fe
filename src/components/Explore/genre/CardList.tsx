82d856aba72f4bfc35660b5029f3680ea35d049a
khangng72 <khangnguyn07022002@gmail.com>
2025-04-30 01:21:30 +0700

    feat: redesign explore/genre

--- src/components/Explore/genre/CardList.tsx
+++ src/components/Explore/genre/CardList.tsx
@@ -1,140 +1,110 @@
-import React from "react";
-import { Eye } from "lucide-react";
-import { Star } from "lucide-react";
-import { TableOfContents } from "lucide-react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import React, { useState } from 'react';
+import { get_books } from '@/app/api/api';
+import { Link } from '@/i18n/routing';
+import Image from 'next/image';
+import { EyeIcon, PlusIcon, StarIcon, TableOfContents } from 'lucide-react';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { get_books } from "@/app/api/api";
-import { Button } from "@/components/ui/button";
-import { Link } from "@/i18n/routing";
+const readingLists = [
+  { id: 1, title: 'Read list title 1' },
+  { id: 2, title: 'Read list title 2' },
+  { id: 3, title: 'Read list title 3' },
+];
 
 const CardList = () => {
+  const [selectedListId, setSelectedListId] = useState<number | null>(null);
+
   const get_all_books = () => {
     return get_books();
   };
 
-  const books_list = get_all_books();
+  const book_list = get_all_books();
   return (
     <div
       className="grid gap-5 justify-center mt-6 
-                grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 px-[20px] xl:px-[50px] 2xl:px-[100px]"
+                grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 w-[90vw] xl:w-[80vw] mx-auto"
     >
-      {books_list.map((book) => (
-        <Dialog key={book.id}>
-          <DialogContent>
-            <div className="flex items-center gap-4">
-              <img
-                src={book.cover}
-                alt="book cover"
-                className="w-[120px] sm:w-[200px]"
-              />
+      {book_list.map((book) => {
+        return (
+          <div
+            key={book.id}
+            className="flex flex-col justify-center items-center bg-card rounded-md p-4 relative"
+          >
+            <Image
+              src={book.cover}
+              alt={book.title}
+              width={200}
+              height={300}
+              className="rounded-lg mb-4 w-[150px] h-[210px]"
+            />
 
-              <DialogHeader className="text-left">
-                <DialogTitle className="text-2xl">{book.title}</DialogTitle>
-                <div>
-                  <Link href="/pages/main/creator/123">by {book.Author}</Link>
-                  <div className="flex gap-3 pb-2">
-                    <div className="flex gap-1">
-                      <Eye className="w-5 h-5" />
-                      <span>{book.view_rate}</span>
-                    </div>
-                    <div className="flex gap-1">
-                      <Star className="w-5 h-5" />
-                      <span>{book.star_rate}</span>
-                    </div>
-                    <div className="flex gap-1">
-                      <TableOfContents className="w-5 h-5" />
-                      <span>{book.number_of_chapters}</span>
-                    </div>
-                  </div>
-                  <DialogDescription>
-                    <span>{book.description}</span>
-                  </DialogDescription>
-                  <ul className="grid grid-cols-2 sm:grid-cols-3 gap-2 mt-3">
-                    {book.genre.map((gen) => (
-                      <li key={gen}>
-                        <span className="text-sm bg-secondary-foreground rounded-sm text-background px-2 m-auto">
-                          {gen}
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </DialogHeader>
+            <Link href="#">
+              <h2 className="text-xl font-semibold hover:underline">
+                {book.title}
+              </h2>
+            </Link>
+            <Link href="#" className="text-muted-foreground hover:underline">
+              by {book.Author}
+            </Link>
+            <div className="flex gap-2 justify-center items-center">
+              <div className="flex gap-1 items-center">
+                <EyeIcon className="w-4 h-4 text-muted-foreground" />
+                <span className="text-sm text-muted-foreground">4</span>
+              </div>
+              <div className="flex gap-1 items-center">
+                <TableOfContents className="w-4 h-4 text-muted-foreground" />
+                <span className="text-sm text-muted-foreground">5</span>
+              </div>
             </div>
+            <div className="flex gap-2 items-center mt-2">
+              <StarIcon className="w-4 h-4" />
+              <StarIcon className="w-4 h-4" />
+              <StarIcon className="w-4 h-4" />
+              <StarIcon className="w-4 h-4" />
+              <StarIcon className="w-4 h-4" />
+            </div>
+            <div className="mt-3 w-[90%] text-justify text-muted-foreground bg-secondary px-4 py-2 text-sm rounded-tl-3xl rounded-br-3xl h-[95px]">
+              <p>{book.description.slice(0, 255)}...</p>
+            </div>
+            <button className="mt-4 px-3 py-1 bg-rainbow hover:opacity-80 rounded-md text-sm">
+              Read Now
+            </button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <button className="absolute -top-3 -right-2 bg-secondary hover:bg-muted-foreground rounded-full p-1 border-[1px] border-foreground">
+                  <PlusIcon />
+                </button>
+              </DialogTrigger>
+              <DialogContent>
+                <div className="flex flex-col gap-2">
+                  <h3 className="text-xl font-semibold">
+                    Add to your Read lists
+                  </h3>
 
-            <DialogFooter>
-              <Button type="submit">Read Now</Button>
-            </DialogFooter>
-          </DialogContent>
-
-          <DialogTrigger className="text-left">
-            <Card className="flex pl-3 h-[320px] items-center">
-              <div className="w-[400px] h-[300px] flex justify-center items-center">
-                <img
-                  src={book.cover}
-                  alt="book cover"
-                  className="w-[400px] h-[200px]"
-                />
-              </div>
-              <div className="flex flex-col gap-0">
-                <CardHeader className="mb-0 pb-2">
-                  <CardTitle className="text-xl md:text-2xl">
-                    {book.title}
-                  </CardTitle>
-                  <CardDescription className="hover:underline">
-                    <Link href="/pages/main/creator/123">by {book.Author}</Link>
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="py-0 text-sm">
-                  <div className="flex gap-3 pb-2">
-                    <div className="flex gap-1">
-                      <Eye className="w-5 h-5" />
-                      <span>{book.view_rate}</span>
-                    </div>
-                    <div className="flex gap-1">
-                      <Star className="w-5 h-5" />
-                      <span>{book.star_rate}</span>
-                    </div>
-                    <div className="flex gap-1">
-                      <TableOfContents className="w-5 h-5" />
-                      <span>{book.number_of_chapters}</span>
-                    </div>
+                  <div className="flex flex-col gap-2 border-b-[0.5px] border-muted-foreground pb-4">
+                    {readingLists.map((readingList) => {
+                      return (
+                        <div
+                          className="flex justify-between items-center gap-2 py-2 hover:cursor-pointer hover:bg-secondary px-3 rounded-md"
+                          key={readingList.id}
+                          onClick={() => setSelectedListId(readingList.id)}
+                        >
+                          <span>{readingList.title}</span>
+                          <input
+                            type="radio"
+                            className="w-5 h-5"
+                            checked={selectedListId === readingList.id}
+                          />
+                        </div>
+                      );
+                    })}
                   </div>
-                  <p>{book.description}</p>
-                </CardContent>
-                <CardFooter className="pl-2 flex justify-start gap-1 mt-3">
-                  <span className="text-sm bg-secondary-foreground rounded-sm text-background px-2 m-auto">
-                    Horror
-                  </span>
-                  <span className="text-sm bg-secondary-foreground rounded-sm text-background px-2 m-auto">
-                    Family
-                  </span>
-                  <span className="text-sm bg-secondary-foreground rounded-sm text-background px-2 m-auto">
-                    Adventure
-                  </span>
-                </CardFooter>
-              </div>
-            </Card>
-          </DialogTrigger>
-        </Dialog>
-      ))}
+                </div>
+              </DialogContent>
+            </Dialog>
+          </div>
+        );
+      })}
     </div>
   );
 };
