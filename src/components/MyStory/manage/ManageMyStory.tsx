888f82c2f2fd47eed86771421fc1bec4c365c4bf
khangng72 <khangnguyn07022002@gmail.com>
2025-05-11 21:56:38 +0700

    feat: add link to new story btn

--- src/components/MyStory/manage/ManageMyStory.tsx
+++ src/components/MyStory/manage/ManageMyStory.tsx
@@ -4,6 +4,7 @@ import { Plus } from 'lucide-react';
 import React, { useState } from 'react';
 import PublishedStories from './PublishedStories';
 import AllStories from './AllStories';
+import { Link } from '@/i18n/routing';
 
 const tabList = [
   { name: 'Published', id: 'published' },
@@ -18,10 +19,13 @@ const ManageMyStory = () => {
         <h1 className="text-xl xl:text-2xl font-bold bg-rainbow text-transparent bg-clip-text">
           My Stories
         </h1>
-        <button className="px-2 py-1 rounded-md text-sm bg-purpleRainbow active:scale-95 transition-all duration-200 ease-in-out text-white flex gap-1 items-center">
+        <Link
+          href={`/mystory/new`}
+          className="px-2 py-1 rounded-md text-sm bg-purpleRainbow active:scale-95 transition-all duration-200 ease-in-out text-white flex gap-1 items-center"
+        >
           <Plus size={16} />
           <span>Add New Story</span>
-        </button>
+        </Link>
       </div>
 
       <div className="flex flex-col bg-card mt-2 rounded-md pb-3">
