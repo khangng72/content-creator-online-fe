4538f341846dcca97f3097b2feececccabf6fc63
KhanhNguyenNguci <khanhnguyennlk4198@gmail.com>
2025-05-12 00:40:59 +0700

    remove redundant hostname

--- next.config.mjs
+++ next.config.mjs
@@ -19,10 +19,6 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'covers.openlibrary.org',
             },
-            {
-                protocol: 'https',
-                hostname: 'oaidalleapiprodscus.blob.core.windows.net',
-            },
         ],
     },
 };
