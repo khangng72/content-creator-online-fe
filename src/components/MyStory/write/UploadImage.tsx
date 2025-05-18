ac3f2da669cd8254ab8a6b498b97bcd552796db8
khangng72 <khangnguyn07022002@gmail.com>
2025-05-18 11:04:07 +0700

    integrate generate AI for chapter

--- src/components/MyStory/write/UploadImage.tsx
+++ src/components/MyStory/write/UploadImage.tsx
@@ -13,6 +13,7 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 import { generateApi, UPDATE_IMAGE } from '@/constants/api';
 import { toast } from '@/hooks/use-toast';
+import { b64JsonToFile } from '@/utils/b64JsonToFile';
 
 interface UploadImageProps {
   chapterId: string;
@@ -31,6 +32,8 @@ const UploadImage = ({
   const [previewImage, setPreviewImage] = useState<string | null>(null);
   const [uploadFile, setUploadFile] = useState<File | null>(null);
   const [open, setOpen] = useState(false);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [prompt, setPrompt] = useState<string>('');
 
   const triggerUploadFile = () => {
     fileInputRef?.current?.click();
@@ -109,6 +112,81 @@ const UploadImage = ({
       });
   };
 
+  const handleGenerateWithAI = async () => {
+    if (!prompt) {
+      toast({
+        title: 'Error',
+        description: 'Please provide a description for AI generation.',
+        duration: 2000,
+        variant: 'destructive',
+      });
+      return;
+    }
+
+    const token = Cookies.get('token');
+
+    setIsGenerating(true);
+
+    try {
+      const introPrompt = `Generate a image with this description. `;
+
+      const response = await axios.post('/api/ai/generate-image', {
+        prompt: introPrompt + prompt,
+        model: 'gpt-image-1',
+        size: '1536x1024',
+      });
+
+      const base64Img = response.data.b64_json;
+      const file = b64JsonToFile(
+        base64Img,
+        `${chapterTitle.replace(/\s+/g, '_')}_img.png`
+      );
+
+      setPreviewImage(URL.createObjectURL(file));
+
+      const formFile = new FormData();
+      formFile.append('file', file as File);
+
+      const uploadFileResult = await axios
+        .post('/api/upload', formFile, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .catch((error) => {
+          console.log('Error', error);
+        });
+
+      await axios.put(
+        generateApi(UPDATE_IMAGE, chapterId),
+        {
+          chapterImageUri: uploadFileResult?.data.path || null,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      fetchChapter();
+    } catch (error) {
+      console.error('Error generating image:', error);
+      toast({
+        title: 'Error',
+        description: 'Failed to generate image',
+        duration: 2000,
+        variant: 'destructive',
+      });
+    } finally {
+      setIsGenerating(false);
+      toast({
+        title: 'Image generated successfully',
+        description: 'Your image has been generated successfully.',
+        duration: 2000,
+      });
+    }
+  };
+
   useEffect(() => {
     if (chapterImageUri) {
       setPreviewImage(chapterImageUri);
@@ -145,6 +223,17 @@ const UploadImage = ({
               storyTitle={chapterTitle}
               className="w-[300px] h-[200px]"
             />
+
+            {isGenerating ? (
+              <div>Generating... please wait until image generation finish</div>
+            ) : (
+              <textarea
+                className="w-[100%] rounded-md px-3 py-2 text-sm resize-none border border-accent focus:outline-none"
+                placeholder="Please describe the image for AI generation"
+                rows={4}
+                onChange={(e) => setPrompt(e.target.value)}
+              />
+            )}
             <div className="flex gap-1 justify-center text-sm sm:text-base mt-2">
               <button
                 className="bg-foreground text-background px-3 py-1 rounded-md flex gap-1 items-center"
@@ -161,7 +250,11 @@ const UploadImage = ({
                 accept="image/*"
                 onChange={handleFileChange}
               />
-              <button className="flex px-3 py-1 bg-rainbow active:scale-95 rounded-md relative text-white">
+
+              <button
+                className="flex px-3 py-1 bg-rainbow active:scale-95 rounded-md relative text-white"
+                onClick={handleGenerateWithAI}
+              >
                 <span>Generate With AI</span>
                 <Sparkles
                   fill="yellow"
