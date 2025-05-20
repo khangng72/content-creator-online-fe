042883bdf5ec54d98c643b0ef77766f667815c8e
khangng72 <khangnguyn07022002@gmail.com>
2025-05-20 21:38:17 +0700

    attach link

--- src/components/AuthorizedHome/StoryPostDialog.tsx
+++ src/components/AuthorizedHome/StoryPostDialog.tsx
@@ -175,9 +175,17 @@ const StoryPostDialog = ({
 
         <div className="w-full block mt-5">
           <div className="flex flex-col gap-3">
-            <h2 className="text-2xl font-bold">Story: {post.storyTitle}</h2>
+            <Link
+              href={`/stori/${post.storyId}`}
+              className="text-2xl font-bold hover:underline"
+            >
+              Story: {post.storyTitle}
+            </Link>
 
-            <Link href="#" className="hover:underline">
+            <Link
+              href={`/chapter/read/${post.chapterId}`}
+              className="hover:underline"
+            >
               <h3 className="text-xl font-semibold">
                 Chapter {post.chapterNumber}: {post.chapterTitle}
               </h3>
